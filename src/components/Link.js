5cf53d843476e20c73bffbfdf91337a28b407e8e
enyichiaagu <enyichiaagu@gmail.com>
2021-09-09 03:09:57 +0100

    commit

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,24 +4,35 @@ import Button from './Button'
 import './Link.css'
 
 function Link({ id, title, url, options, notClickable }) {
+
+    let faviconLink;
+    faviconLink = url.replace('https://', '')
     return (
         <div className="link-field">
-            <div className="link-panel">
-                {
-                    notClickable ?
-                        <div>
-                            <p className="title">{ title }</p>
-                        </div> :
-                        <div>
-                            <a href={url} className="title" target="_blank" rel="noreferrer">{ title }</a>
-                            <img src={linkIcon} alt="Link Icon" className="svg-style"/>
-                        </div>
-                }
+            {
+                !notClickable &&
+                <div style={{marginRight: '1em'}}>
+                    <img src={`https://api.faviconkit.com/${faviconLink}/50`} alt="logo" height="50px"/>
+                </div> 
+            }
+            <div>
+                <div className="link-panel">
+                    {
+                        notClickable ?
+                            <div>
+                                <p className="title">{ title }</p>
+                            </div> :
+                            <div>
+                                <a href={url} className="title" target="_blank" rel="noreferrer">{ title }</a>
+                                <img src={linkIcon} alt="Link Icon" className="svg-style"/>
+                            </div>
+                    }
+                </div>
+                <p className="url">{ url }</p>
             </div>
-            <p className="url">{ url }</p>
             {
                 options &&
-                <div style={{display: "flex", alignItems: "center", marginTop: "1em"}}>
+                <div style={{display: "flex", alignItems: "center", marginTop: "1em", flexBasis: '100%'}}>
                     <Button text="Edit" type="ghost" goto={"/edit/"+ id}/>
                     <Button text="Delete" type="danger" goto={"/delete/" + id } style={{marginLeft: "1em"}}/>
                 </div>
